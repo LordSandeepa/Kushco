5d5559ccbea0382c78abd587567b0e400a297249
Sandeepa Bandara <108658788+LordSandeepa@users.noreply.github.com>
2024-07-01 12:24:37 +0530

    cre

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,9 +24,3 @@ for (let i = 0; i < navElemArr.length; i++) {
  * add active class on header when scrolled 200px from top
  */
 
-const header = document.querySelector("[data-header]");
-
-window.addEventListener("scroll", function () {
-  window.scrollY >= 200 ? header.classList.add("active")
-    : header.classList.remove("active");
-})
